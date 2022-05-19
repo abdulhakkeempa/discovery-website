885b6058ff99c5647275fc9cd5cd87ecd1b0c4ab
Abdul Hakkeem P A <92361680+hakkeempa@users.noreply.github.com>
2022-05-19 22:50:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var typed = new Typed(".auto-input", {
-    strings: ["Trust book recomendations from real people , not robots &#128512;"],
+    strings: ["Trust book recomendations from real people , not robots &#129302;"],
     typeSpeed:115,
     showCursor:false,
     })
+
+    scrollButton = document.getElementById("scrollBtn");
+
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+        scrollButton.style.display = "block";
+      } else {
+        scrollButton.style.display = "none";
+      }
+    }
+    
+    function triggerFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0; 
+    }
